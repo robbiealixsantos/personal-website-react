0395fd955c0be466340ca5289ce0c4b6bb7ab4f9
Robbie <robbiealixsantos@gmail.com>
2019-02-13 15:11:37 -0800

    update projects section

--- src/components/ProjectsAndResume.jsx
+++ src/components/ProjectsAndResume.jsx
@@ -11,6 +11,7 @@ class ProjectsAndResume extends Component {
                         <a href="http://klinkapp.herokuapp.com/" className="btn btn-default btn-lg">KLINK</a>
                         <a href="http://repod.herokuapp.com/" className="btn btn-default btn-lg">Repo/d</a>
                         <a href="https://nostalgic-bhabha-63c0a7.netlify.com/" className="btn btn-default btn-lg">Tosche Station v1 - React App</a>
+                        <a href="https://github.com/robbiealixsantos/personal-website-react" className="btn btn-default btn-lg">Github code for this Website - React Conversion from HTML5/CSS/Bootstrap theme</a>
                         <a href="https://github.com/robbiealixsantos/restricted-call-dates" className="btn btn-default btn-lg">Restricted Call Dates - Nuget package for Contact Center TCPA & DNC Compliance</a>
                         <a href="https://drive.google.com/open?id=0BzYK0mNiCUWsQXJjSHBtbU5RSVU" className="btn btn-default btn-lg">Resume</a>
                     </div>
