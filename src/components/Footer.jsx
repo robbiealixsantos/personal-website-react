6d02182b85e0b0cb3d639d6c24ce97da4b37d752
Robbie <robbiealixsantos@gmail.com>
2019-02-13 14:49:34 -0800

    converstion to react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+            <div className="container text-center">
+                <p>Copyright &copy; Robbie Santos - 2019</p>
+                <p>This page was made with React <img alt="react logo" src="/assets/img/logo.png"/></p>
+            </div>
+        </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/ProjectsAndResume.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ProjectsAndResume extends Component {
+    render() {
+        return (
+            <section id="resume" className="content-section text-center">
+            <div className="download-section">
+                <div className="container">
+                    <div className="col-lg-8 col-lg-offset-2">
+                        <h2>Projects and Resume</h2>
+                        <a href="http://klinkapp.herokuapp.com/" className="btn btn-default btn-lg">KLINK</a>
+                        <a href="http://repod.herokuapp.com/" className="btn btn-default btn-lg">Repo/d</a>
+                        <a href="https://nostalgic-bhabha-63c0a7.netlify.com/" className="btn btn-default btn-lg">Tosche Station v1 - React App</a>
+                        <a href="https://github.com/robbiealixsantos/restricted-call-dates" className="btn btn-default btn-lg">Restricted Call Dates - Nuget package for Contact Center TCPA & DNC Compliance</a>
+                        <a href="https://drive.google.com/open?id=0BzYK0mNiCUWsQXJjSHBtbU5RSVU" className="btn btn-default btn-lg">Resume</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        );
+    }
+}
+
+export default ProjectsAndResume;
