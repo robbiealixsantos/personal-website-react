00a7bfdd87b36d532e60edb73ceaf3800ca15fc7
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2019-02-21 17:51:34 -0800

    add deployed heroku url to project

--- src/components/ProjectsAndResume.jsx
+++ src/components/ProjectsAndResume.jsx
@@ -10,7 +10,7 @@ class ProjectsAndResume extends Component {
                         <h2>Projects and Resume</h2>
                         <a href="http://klinkapp.herokuapp.com/" className="btn btn-default btn-lg">KLINK</a>
                         <a href="http://repod.herokuapp.com/" className="btn btn-default btn-lg">Repo/d</a>
-                        <a href="https://github.com/robbiealixsantos/coruscant-connect" className="btn btn-default btn-lg">Coruscant Connect - A social network for the planet Coruscant built on the MERN stack</a>
+                        <a href="https://coruscant-connect.herokuapp.com" className="btn btn-default btn-lg">Coruscant Connect - A social network for the planet Coruscant built on the MERN stack</a>
                         <a href="https://nostalgic-bhabha-63c0a7.netlify.com/" className="btn btn-default btn-lg">Tosche Station v1 - React App</a>
                         <a href="https://github.com/robbiealixsantos/personal-website-react" className="btn btn-default btn-lg">Github code for this Website - React Conversion from HTML5/CSS/Bootstrap theme</a>
                         <a href="https://github.com/robbiealixsantos/restricted-call-dates" className="btn btn-default btn-lg">Restricted Call Dates - Nuget package for Contact Center TCPA & DNC Compliance</a>
