84474cb5e24f89ea916cacdf3d4509a66b8d9c64
Robbie Santos <robbiealixsantos@gmail.com>
2019-05-05 20:37:47 -0700

    update profile photo url

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ class About extends Component {
         return (
         <section id="about" className="container content-section text-center">
             <div className="row">
-                <div className="col-md-4"><img alt="my face" src="https://drive.google.com/open?id=0BzYK0mNiCUWsVEl1emF4Y1lNaEU"/></div>
+                <div className="col-md-4"><img alt="my face" src="https://media.licdn.com/dms/image/C4D03AQHOLLK3snbtiw/profile-displayphoto-shrink_200_200/0?e=1562803200&v=beta&t=2wFjE-ru_cyQ16phn7F4-jzsxbfOUrIq8GsErLNxqPo"/></div>
                 <div className="col-md-8">
                     <h2>About Me</h2>
                         <p>Hello there! I am a former political science major and jack-of-all-trades turned solutions engineer and full-stack web developer. 
