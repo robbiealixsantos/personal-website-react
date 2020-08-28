552314555ab1d2c01f4eabfdad1822bc6d422aa0
Robbie Santos <robbiealixsantos@gmail.com>
2020-08-27 21:32:11 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ class About extends Component {
         return (
         <section id="about" className="container content-section text-center">
             <div className="row">
-                <div className="col-md-4"><img alt="my face" src="https://photos.google.com/photo/AF1QipP_AMMTfdgzc1hWcPHl2xthmPE9U_zyBUX5_WtY"/></div>
+                <div className="col-md-4"><img alt="my face" src="https://i.ibb.co/HzqddZt/robbie-face.png"/></div>
                 <div className="col-md-8">
                     <h2>About Me</h2>
                         <p>Hello there! I am a former political science major and jack-of-all-trades turned solutions engineer and full-stack web developer. 
